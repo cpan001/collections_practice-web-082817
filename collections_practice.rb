db802025ad658f63dab2e60f2399d047b40c8b78
cpan001 <github email address>
2017-08-28 17:57:14 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,50 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(arr_of_strs)
+  arr_of_strs.sort_by { |str| str.length }
+end
+
+def swap_elements(arr)
+  arr[1],arr[2] = arr[2], arr[1]
+  arr
+end
+
+def swap_elements_from_to(arr, index, destination_index)
+  arr[index], arr[destination_index] = arr[destination_index], arr[index]
+  arr
+end
+
+puts swap_elements_from_to(["a", "b", "c"],0,2).inspect
+
+def reverse_array(arr_of_ints)
+  arr_of_ints.reverse
+end
+
+def kesha_maker(arr_of_strs)
+  arr_of_strs.map { |str|
+    str[2] = "$"
+    str
+  }
+end
+
+def find_a(arr_of_strs)
+  arr_of_strs.select { |str|
+    str.start_with?("a")
+  }
+end
+
+def sum_array(arr_of_ints)
+  arr_of_ints.inject(:+)
+end
+
+def add_s(arr_of_strs)
+  arr_of_strs.each_with_index.collect { |str, i|
+    i != 1 ? str+"s" : str
+  }
+end
--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,14 @@ describe 'collections practice' do
     end
   end
 
+  # Question 4B
+  describe '#swap_elements_from_to' do
+    it 'swap the element at index of an array to destination index and vice versa' do
+      expect(swap_elements_from_to(["a", "b", "c"], 0, 2)).to eq(["c", "b", "a"])
+      expect(swap_elements_from_to(["a", "b", "c"], 2, 1)).to eq(["a", "c", "b"])
+    end
+  end
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array' do
